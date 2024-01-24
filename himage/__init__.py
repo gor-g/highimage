9a863cbdf5c62bb6967509831dfa1a070053300c
Gor G <gg@gorg.fr>
2024-01-24 21:29:05 +0100

    fixed imports after removing untested code from the main branch

--- himage/__init__.py
+++ himage/__init__.py
@@ -1,6 +1,2 @@
 import himage.plot as plot
 from .plot.plot import imshow, multimshow
-import himage.transform as transform
-from .transform import rescale, rotate, standardize
-import himage.describe as describe
-from .describe.describe import nonAngularContourSignature
