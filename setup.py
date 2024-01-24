18b0625cb8e27a8a51b0052fc9f6bbc2200bbb38
Gor G <gg@gorg.fr>
2024-01-24 21:18:32 +0100

    Add setup.py file for project configuration

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='himage',
+    version='0.0.3',
+    description='A small library, of high level image processing tools',
+    url='https://github.com/mySpecialUsername/highImage/',
+    author='Gor G.',
+    packages=find_packages(),
+    install_requires=['numpy', 'opencv-python', 'matplotlib', 'PyQt6'],
+    python_requires='>=3.6',
+    zip_safe=False
+)
