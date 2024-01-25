1b945414c40e004bb705a2ddc19bcbe6c81fc714
Gor G <gg@gorg.fr>
2024-01-25 18:39:17 +0100

    updated version in setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as f:
 
 setup(
     name='himage',
-    version='0.1.1',
+    version='0.1.2',
     description='A small library, of high level image processing tools',
     url='https://github.com/mySpecialUsername/highImage/',
     author='Gor G.',
